d0a4596ccdb37cf33ebb8d5e1455d4cdc9938b0b
RH-sdavey <32485509+RH-sdavey@users.noreply.github.com>
2020-02-07 17:00:40 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -12,7 +12,8 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin git@github.com:RH-sdavey/tieto_lunch.git > /dev/null 2>&1
+  git remote add origin RH-sdavey:$lunch_pass@github.com:RH-sdavey/tieto_lunch.git > /dev/null 2>&1
+  echo " git remote add finshed with  : $? "
   git push --quiet --set-upstream origin master
 }
 
